b4b0493554c7147577af7fbeffc3571a373a78a5
cesarrafaeljunior <cesarrdev@gmail.com>
2022-10-07 00:02:15 -0300

    feat Login User

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -10,7 +10,7 @@ export const RoutesMain = () => {
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<Navigate to="login" />} />
       <Route path="/register" element={<Register />} />
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 };
